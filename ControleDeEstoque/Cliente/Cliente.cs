4b64f62f3d25fd4097b73d7279d1f4ce9fa63e33
mmurilo03 <murilo.ferreira@academico.ifpb.edu.br>
2023-09-20 20:03:57 -0300

    Enviando projeto de padrões em C#

--- ControleDeEstoque/Cliente/Cliente.cs
+++ ControleDeEstoque/Cliente/Cliente.cs
@@ -3,12 +3,10 @@ namespace ControleDeEstoque;
 public abstract class Cliente : ICliente
 {
     public string Nome { get; set; }
-    public List<Compra> Compras { get; }
 
     public Cliente(string nome)
     {
         Nome = nome;
-        Compras = new List<Compra>();
     }
 
     public abstract double Pagar(Compra compra);
--- ControleDeEstoque/Cliente/ICliente.cs
+++ ControleDeEstoque/Cliente/ICliente.cs
@@ -3,5 +3,4 @@ namespace ControleDeEstoque;
 public interface ICliente
 {
     public string Nome { get; set; }
-    public List<Compra> Compras { get; }
 }
